a27aa1f4e769d6eba4878c4aab22c84c6319a24c
Josh Kennedy <josh.kennedy@artisantechgroup.com>
2021-05-03 11:32:18 -0500

    Initial Commit

---
+++ SwiftToDo/SwiftToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftToDoApp.swift
+//  SwiftToDo
+//
+//  Created by Josh Kennedy on 5/3/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
